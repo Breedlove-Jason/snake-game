e8a99682a1ce791587bab653504b1d09edc4516d
Jason Breedlove <j@jasonbreedlove.com>
2021-11-25 13:58:47 -0800

    after snake index function for increasing the tail size when snake eats food

--- snake_game.py
+++ snake_game.py
@@ -40,3 +40,19 @@ score_font = pygame.font.SysFont("comicsansms", 35)
 def your_score(score):
     value = score_font.render("Your Score: " + str(score), True, yellow)
     dis.blit(value, [0, 0])
+
+
+# create our snake
+def our_snake(snake_block, snake_list):
+    # loop through the variable snake_list to increase the size of the tail after snake eats food
+    for x in snake_list:
+        # draw the snake on the display with an increase of 10 as defined in the snake variable
+        # rectangle = pygame.Rect(display, color, x_pos, y_pos, width, height)
+        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
+
+
+# create the message
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    # center the message on the screen by dividing the width and the height
+    dis.blit(mesg, [dis_width / 6, dis_height / 3])
